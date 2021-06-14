977c8747240031d35f69036feaa24f5c71a06c66
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-14 16:55:32 -0400

    trello

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,1 +1,8 @@
-console.log("movielist");
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+const App = () => {
+  return <h1>MovieList</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- movies/webpack.config.js
+++ movies/webpack.config.js
@@ -1,3 +1,9 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
 module.exports = {
   module: {
     rules: [
@@ -5,9 +11,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: '"babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
   },
+  plugins: [htmlPlugin],
 };
