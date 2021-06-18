07df0fa76410f8f3e3875fdc6de415a7a65d4c95
mike <michaelwilsonsj@hotmail.com>
2021-06-18 18:06:06 -0400

    added column and card components rendered on app tsx

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Column } from './Column'
+import { Card } from './Card'
 import { AppContainer } from './styles'
 import './App.css';
 
 function App() {
   return (
-    <AppContainer></AppContainer>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+    </AppContainer>
   );
 }
 
--- trello-clone/src/Column.tsx
+++ trello-clone/src/Column.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { ColumnContainer, ColumnTitle } from './styles'
 
-export const Column = () => {
-    return <div>Column Title</div>
+interface ColumnProps {
+    text: string
+}
+
+export const Column = ({ text, children }: React.PropsWithChildren<ColumnProps>) => {
+    return(
+        <ColumnContainer>
+            <ColumnTitle>{text}</ColumnTitle>
+            {children}
+        </ColumnContainer>
+    ) 
 }
