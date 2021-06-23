cdab9da575cd59c2d33ad14a681f0e7e0fd7413d
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-23 13:16:50 -0400

    expanding accordion - learning state management

--- widgets/widgets/src/components/Accordion.js
+++ widgets/widgets/src/components/Accordion.js
@@ -8,9 +8,10 @@ const Accordion = ({ items }) => {
   };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div onClick={() => onTitleClick(index)} className="title active">
+        <div onClick={() => onTitleClick(index)} className={`title ${active}`}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
