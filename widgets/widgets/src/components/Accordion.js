78cad5e7c4f6b64fe8449128eac59e8b2a2f77ab
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-23 10:24:09 -0400

    adding event listener to accordion title sec142

--- widgets/widgets/src/components/Accordion.js
+++ widgets/widgets/src/components/Accordion.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
-        return (
-            <div key={item.title}>
-                <div className="title active">
-                    <i className="dropdown icon"></i>
-                    {item.title}
-                </div>
-                <div className="content active">
-                    <p>{item.content}</p>
-                </div>
-            </div>
-        )
-    })
+  const renderedItems = items.map((item) => {
     return (
-        <div className="ui styled accordion">
-            {renderedItems}
+      <React.Fragment key={item.title}>
+        <div
+          onClick={() => console.log("title clicked")}
+          className="title active"
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
         </div>
-    )
-}
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
 
 export default Accordion;
