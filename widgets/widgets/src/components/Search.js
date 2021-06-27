7f2e0bc27b96ea00cacd16e4f589c8d576cc4d61
mike <michaelwilsonsj@hotmail.com>
2021-06-27 16:18:51 -0400

    rendering search results to jsx

--- widgets/widgets/src/components/Search.js
+++ widgets/widgets/src/components/Search.js
@@ -21,6 +21,19 @@ const Search = () => {
       search();
   }, [term])
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          {result.snippet}
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div>
       <div className="ui form">
@@ -31,6 +44,9 @@ const Search = () => {
           onChange={e => setTerm(e.target.value)}
           className="input" />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
